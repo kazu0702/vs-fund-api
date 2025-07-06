efe65633c0dd7ae46936cc82abd92694a7830d82
kazu0702 <xxxkmhyxxx@gmail.com>
2025-07-06 22:33:00 +0900

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router  = express.Router();
 
+router.get("/ping", (_, res) => res.json({ pong: true })); 
 router.use("/billing",      require("./billing"));
-router.use("/emailChange",  require("./emailChange"));   // ★ 追加
+router.use("/emailChange",  require("./emailChange")); 
 
 module.exports = router;
