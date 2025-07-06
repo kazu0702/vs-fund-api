bd440b73f6a3b79462c3ba161cb5bf755370ecc7
kazu0702 <xxxkmhyxxx@gmail.com>
2025-07-06 23:06:57 +0900

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const router  = express.Router();
+const router = require("express").Router();
 
 router.get("/ping", (_, res) => res.json({ pong: true })); 
 router.use("/billing",      require("./billing"));
